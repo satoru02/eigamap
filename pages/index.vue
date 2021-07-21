314e5cdcd016e4843d6fdb6d23d32b3171da24aa
satoru02 <sugiwaka02@gmail.com>
2021-07-21 17:01:10 +0900

    #1 install mapbox

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="map">
+
+  </div>
+</template>
+
+<script>
+import mapboxgl from 'mapbox-gl';
+export default {
+  data(){
+    return {
+      access_token: process.env.MAPBOX_TOKEN,
+      map: {}
+    }
+  },
+  mounted(){
+    this.createMap()
+  },
+  methods: {
+    createMap(){
+      mapboxgl.accessToken = this.access_token
+      this.map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        zoom: 11,
+        center: [-87.661557, 41.893748]
+      })
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+#map {
+   width:100%;
+   height:100vh;
+}
+</style>
