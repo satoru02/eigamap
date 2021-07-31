186c9cb6f1410ad65d046b6c7daec6b0c15aa82d
satoru02 <sugiwaka02@gmail.com>
2021-07-31 22:30:36 +0900

    #19 add date selector

--- components/InfoSection.vue
+++ components/InfoSection.vue
@@ -1,16 +1,16 @@
 <template>
   <section>
-    <ul v-if="this.info">
-      <li v-for="(day, index) in daysOfWeek" :key="index" @click="changeDays(day)" class="mr-3 mt-2 cursor-pointer">
-        <button class="text-base text-gray-400 focus:text-white font-medium">{{ daysToWeekend(index) }}</button>
-      </li>
-    </ul>
+    <select v-if="this.info" v-model="selectedDay" class="rounded-sm text-xl mt-3 font-semibold bg-black mb-3 text-gray-300">
+      <option v-for="(day, index) in daysOfWeek" :key="index" :value="index">
+        {{ daysToWeekend(index) }}
+      </option>
+    </select>
     <div class="mt-3 bg-white sm:rounded-lg p-6" v-for="(movie,index) in info[0]" :key="index">
       <h2 class="text-lg leading-7 font-semibold">
         {{ movie.title }}
       </h2>
       <div class="hover:bg-blue-400 cursor-pointer border border-gray-300 rounded-3xl mt-4 text-lg text-current"
-        v-for="(date, index) in movie.props[0][cnmDayCount]" :key="index">
+        v-for="(date, index) in movie.props[0][selectedDay]" :key="index">
         <p class="ml-14">{{ date.time }} </p>
       </div>
     </div>
@@ -23,7 +23,7 @@
     data() {
       return {
         daysOfWeek: [],
-        cnmDayCount: 0
+        selectedDay: 0
       }
     },
     props: {
@@ -35,8 +35,6 @@
     created() {
       this.daysOfWeek = this.setDaysToWeekend()
     },
-    computed: {
-    },
     methods: {
       setDaysToWeekend() {
         let today = new Date(Date.now())
@@ -47,34 +45,35 @@
         }
         return ary
       },
-      setTargetDay(targetDay){
+      setTargetDay(targetDay) {
         let day = new Date()
         day.setDate(day.getDate() + targetDay)
         return day
       },
       daysToWeekend(daysIndex) {
-        let options = {　month: 'long',　day: 'numeric'}
+        let options = {
+          month: 'long',
+          day: 'numeric'
+        }
         let day = this.setTargetDay(daysIndex)
         return new Date(day).toLocaleDateString('ja', options)
       },
-      changeDays(targetDay){
-        this.cnmDayCount = targetDay
-      },
     }
   }
+
 </script>
 
 <style lang="postcss">
-ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 5%;
-  overflow: hidden;
-}
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 5%;
+    overflow: hidden;
+  }
 
-li {
-  float: left;
-}
+  li {
+    float: left;
+  }
 
   .new_badge {
     @apply border border-gray-300 rounded-lg px-3 py-1 text-lg font-semibold text-current;
@@ -83,4 +82,5 @@ li {
       @apply bg-gray-300;
     }
   }
+
 </style>
